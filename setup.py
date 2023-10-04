4ff1cc329992be4bfa6b3bbeacd007bbab277198
ahmedali <ahmed@ajaali.com>
2023-10-04 11:55:00 +0100

    Moving to version 1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def readme():
 
 setup(
     name='leafy',
-    version='0.1.5',
+    version='1.0.0',
     description='Another fast graph algorithms library',
     long_description=readme(),
     long_description_content_type='text/markdown',
